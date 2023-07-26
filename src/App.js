d9934c543a428ca717e94136cdc33204f289c2f3
Haris A <108242470+haristiks@users.noreply.github.com>
2023-07-26 15:04:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
-import Nopage from "./pages/Nopage";
 import { Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
@@ -9,11 +8,11 @@ import './Pages.css'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path='/' element={<Landing />} />
       <Route path='/home' element={<Home />} />
       <Route path="/registration" element={<Registration />} />
       <Route path="/login" element={<Login />} />
-      <Route path="*" element={<Nopage />} />
+    
     </Routes>
   );
 }
